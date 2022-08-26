1b158e1171079265d088362cd5af315b5ea65c80
João Pedro <joaopedrobcaetano@gmail.com>
2022-08-25 23:34:03 -0300

    fazendo desafios

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,10 @@
 let nome = "João Pedro"
-let email = "joaopedrobcaetano@gmail.com"
+let email = "teste@teste.com"
 let profissao = "Programador"
 let idade = 29
 let local = "Liverpool"
+
+console.log(nome, email, profissao, idade, local)
+console.log(`Meu nome é ${nome}, meu email é ${email}, sou ${profissao}, tenho ${idade} anos, quero conhecer ${local}.`)
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${local}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
